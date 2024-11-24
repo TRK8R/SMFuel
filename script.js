9613f9fdbc9f90f2ea79c03e169687ddfa210d58
TRK8R <maba0383@tutamail.com>
2024-11-24 15:27:34 -0600

    Added day of month function

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let timeOffset = 0
 // Get the select element for time zone selection
 const tzSelect = document.getElementById('tz-select');
 
+// Add event listener to the time zone select element
+document.getElementById('tz-select').addEventListener('change', updateTimezone);
+
+// function to get current day of the month
+function getCurrentDay() {
+  const tempDay = new Date();
+  return tempDay.getDate()
+}
+
 // Function to update the timezone based on user input
 function updateTimezone() {
   const selectedTz = tzSelect.value;
@@ -11,6 +20,5 @@ function updateTimezone() {
   console.log(`Time Offset: ${timeOffset}`);
 }
 
-// Add event listener to the time zone select element
-document.getElementById('tz-select').addEventListener('change', updateTimezone);
+
 
